0b09a115409beb177dea0f3068fc192175cfec55
Christopher McCulloh <cmcculloh@salesforce.com>
2018-07-09 11:37:48 -0400

    (INIT) moves views into public for simplicity sake

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -25,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
