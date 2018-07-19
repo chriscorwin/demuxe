535ac45326b547b3af6c3be4a7e0aff3f1e7391d
Christopher McCulloh <cmcculloh@salesforce.com>
2018-07-19 16:08:01 -0400

    (GH7) public -> engine. Introduces the concept of Demuxe engine, template, brand-theme, and public

--- app.js
+++ app.js
@@ -25,18 +25,33 @@ if (process.env.DEBUG === "true") {
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'public'));
+// https://expressjs.com/en/4x/api.html#app.set
+// views are looked up in the order they occur in the array (earlier takes precedence over later --cascade flows reverse of the way it does in CSS)
+app.set('views', [
+  path.join(__dirname, 'public'),
+  path.join(__dirname, 'templates', config.template), 
+  path.join(__dirname, 'engine')
+]);
+
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(expressSanitizer()); // this line follows bodyParser() instantiations
-app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(lessMiddleware(path.join(__dirname, 'brand-themes', config.brand)));
-app.use(express.static(path.join(__dirname, 'brand-themes', config.brand)));
+// https://expressjs.com/en/4x/api.html#app.use
+app.use([
+  logger('dev'),
+  express.json(),
+  express.urlencoded({ extended: false }),
+  expressSanitizer(),
+  cookieParser(),
+  // files are looked up in reverse order they occur in the array (later takes precedence over earlier here --cascade flows like CSS)
+  lessMiddleware(path.join(__dirname, 'engine')),
+  express.static(path.join(__dirname, 'engine')),
+  lessMiddleware(path.join(__dirname, 'brand-themes', config.brand)),
+  express.static(path.join(__dirname, 'brand-themes', config.brand)),
+  lessMiddleware(path.join(__dirname, 'templates', config.template)),
+  express.static(path.join(__dirname, 'templates', config.template)),
+  lessMiddleware(path.join(__dirname, 'public')),
+  express.static(path.join(__dirname, 'public'))
+]);
 
 const router = express.Router();
 
@@ -49,6 +64,9 @@ const router = express.Router();
  * eg: http://your.site.com/kidney/beans, serves the ejs file /public/kidney/beans.ejs
  * or serves /public/404.ejs if that file does not exist.
  * 
+ * If a non-existent static (js/css/html/svg/png/etc) file is requested, it will fall through the
+ * express.static middleware into this. This will handle it and serve an appropriate 404 error.
+ * 
  * All user input will be sanitized. If you have a URL or Query param that is getting mutated unexpectedly, this is why.
  */
 router.get('/*', function(req, res, next) {
@@ -58,17 +76,27 @@ router.get('/*', function(req, res, next) {
       return sanitizedQueryParams;
   }, {});
   const sanitizedURL = req.sanitize(req.params[0]) || 'index';
-  fs.stat(path.resolve(`public/${sanitizedURL}.ejs`), function(err, data) {
-    if (err) {
-      res.render('404', { page: sanitizedURL, ...config, sanitizedQueryParams: sanitizedQueryParams }, (err, html) => {
-        if (req.url.match(/.css$/)) {
-          res.set('Content-Type', 'text/css');
+  console.log(`${sanitizedURL} requested`);
+  let error = true;
+  // Check to see if the file exists in any of the three possible view directories. If not, error.
+  fs.access(path.join(__dirname, 'public', `${sanitizedURL}.ejs`), fs.constants.F_OK | fs.constants.R_OK, (err) => {
+    if (!err) error = false;
+    fs.access(path.join(__dirname, 'templates', `${sanitizedURL}.ejs`), fs.constants.F_OK | fs.constants.R_OK, (err) => {
+      if (!err) error = false;
+      fs.access(path.join(__dirname, 'engine', `${sanitizedURL}.ejs`), fs.constants.F_OK | fs.constants.R_OK, (err) => {
+        if (!err) error = false;
+        if (error) {
+          res.render('404', { page: sanitizedURL, ...config, sanitizedQueryParams: sanitizedQueryParams }, (err, html) => {
+            if (req.url.match(/.css$/)) {
+              res.set('Content-Type', 'text/css');
+            }
+            res.send(html);
+          });
+        } else {
+          res.render(sanitizedURL, { ...config, sanitizedQueryParams: sanitizedQueryParams });
         }
-        res.send(html);
       });
-    } else {
-      res.render(sanitizedURL, { ...config, sanitizedQueryParams: sanitizedQueryParams });
-    }
+    });
   });
 });
 
