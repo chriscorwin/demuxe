2f901f67aaee539baff8ceabb27f54350e2e1bed
Christopher McCulloh <cmcculloh@salesforce.com>
2018-07-19 11:20:22 -0400

    (INIT) fixes errors with variable names and makes prod settings be the default settings

--- config/config.js
+++ config/config.js
@@ -1,25 +1,31 @@
 // Satic data here, so that we do not have to generate the config data all for every environment unless we wanna
-let config_data = null
+let configData = null;
 module.exports = function() {
 	// if the static data was already set. return it
-	if(config_data != null && config_data != undefined) {
-		return config_data
+	if(configData != null && configData != undefined) {
+		return configData;
 	}
-	    
-	config_data = {}
+	
+	// default to production
+	const defaultConfigData = require('./config.production.json');
 
-	// LOAD JSON
-	if(process.env.NODE_ENV === undefined || process.env.NODE_ENV == null || process.env.NODE_ENV == 'dev') { 
-	    config_data = require('./config.dev.json')
-	} else {
-		if(process.env.NODE_ENV == 'qa') {
-		    config_data = require('./config.qa.json')
-		} else {
-		    config_data = require('./config.production.json')
+
+	let envConfigData = {};
+	// obtain env specific config
+	if(process.env.NODE_ENV) {
+		if (process.env.NODE_ENV == 'qa') {
+		    envConfigData = require('./config.qa.json');
+		} else if (process.env.NODE_ENV === 'dev') { 
+			envConfigData = require('./config.dev.json');
 		}
 	}
+
+	// merge default with env config, overwriting defaults
+	configData = { ...defaultConfigData, ...envConfigData };
+
 	// LOAD FROM ENV VARIABLES -- you can set an env variable and this will just catch it. NICE.
-	config_data.SOME_STATIC_VAR = process.env.SOME_STATIC_VAR 
-	config_data.port = process.env.port || config_data.port
-		return config_data
+	configData.SOME_STATIC_VAR = process.env.SOME_STATIC_VAR;
+	configData.port = process.env.port || configData.port;
+	
+	return configData;
 }
