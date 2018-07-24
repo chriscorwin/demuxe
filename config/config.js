d4be5f64b9ec32917c6053ec9fd22c33066a4398
Christopher McCulloh <cmcculloh@salesforce.com>
2018-07-24 12:43:18 -0400

    (GH7) restructuring based on talks with Corwin

--- config/config.js
+++ config/config.js
@@ -9,23 +9,20 @@ module.exports = function() {
 	}
 	
 	// default to production
-	const defaultConfigData = require('./config.production.json');
+	const defaultConfigData = require('./config.json');
 
 
 	let envConfigData = {};
 	// obtain env specific config
 	if(process.env.NODE_ENV) {
-		if (process.env.NODE_ENV == 'qa') {
-		    envConfigData = require('./config.qa.json');
-		} else if (process.env.NODE_ENV === 'dev') { 
-			envConfigData = require('./config.dev.json');
-		}
+		envConfigData = require(`./config.overrides.${process.env.NODE_ENV}.json`) || {};
 	}
 
 	// merge default with env config, overwriting defaults
 	configData = { ...defaultConfigData, ...envConfigData };
-
-	configData.localization = path.join(__dirname, 'brand-themes', configData.brand, 'localization.json');
+console.log('configData');
+console.dir(configData);
+	configData.localization = path.join(__dirname, 'brand-themes', configData.brandTheme, 'localization.json');
 
 	// LOAD FROM ENV VARIABLES -- you can set an env variable and this will just catch it. NICE.
 	configData.SOME_STATIC_VAR = process.env.SOME_STATIC_VAR;
