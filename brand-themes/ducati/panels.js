171519ea25d669ca074930f0cdd5186eb0031ca4
Christopher McCulloh <cmcculloh@salesforce.com>
2018-08-02 13:02:52 -0400

    (GH31) updates panels

--- brand-themes/ducati/panels.js
+++ brand-themes/ducati/panels.js
@@ -11,7 +11,7 @@ module.exports = {
 			subCategory: 'Loyalty'
 		},
 		{
-			title: 'Abandon Cart High Value',
+			title: 'Redline Magazine Subscribers',
 			subTitle: 'Transaction Segment / ID: <b>rsdqtbya</b>',
 			devices: '9.3M',
 			audience: '2.1M',
@@ -21,7 +21,7 @@ module.exports = {
 			subCategory: 'Gender'
 		},
 		{
-			title: 'Clicked on Ad did not convert',
+			title: 'WDW 2018 Ticket Holders',
 			subTitle: 'Standard Segment / ID: <b>rsdqtah9w</b>',
 			devices: '45.3M',
 			audience: '14.2M',
@@ -31,7 +31,7 @@ module.exports = {
 			subCategory: 'Attributes'
 		},
 		{
-			title: 'Crocs Club customer and Purchase last 90 days',
+			title: 'Ducati Loyalists',
 			subTitle: 'Composite Segment / ID: <b>rsdqtbasi</b>',
 			devices: '57.89M',
 			audience: '19.2M',
@@ -61,26 +61,13 @@ module.exports = {
 			subCategory: 'Loyalty'
 		},
 		{
-			title: 'Prospects that have seen media',
+			title: 'Revzilla Customers',
 			subTitle: 'Composite Segment / ID: <b>q825zbzj</b>',
 			devices: '588K',
 			audience: '212K',
 			modified: 'Dec 30, 2017',
 			type: 'Demographic',
 			category: 'Demographic',
-			subCategory: 'Loyalty'
-		}
-	],
-	// These will be added in reverse order listed here
-	additional: [
-		{
-			title: '<span class="slds-badge slds-m-right_small">LOOKALIKE</span><img src="/images/logo-noname.svg" height=10 /> High Value Customers Lookalikes',
-			subTitle: 'Standard Segment / ID: <b>rsdqtbhe8</b> <img src="/images/icons/sort.svg" />',
-			devices: '13.8M',
-			audience: '2.8M',
-			modified: 'August 06, 2018',
-			type: 'Standard',
-			category: 'Behavioral',
 			subCategory: 'Loyalty'
 		}
 	]
