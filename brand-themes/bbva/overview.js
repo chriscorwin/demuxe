95238af829993eea26ca1beefdf306a639eba358
Christopher McCulloh <cmcculloh@salesforce.com>
2018-08-28 15:09:12 -0400

    (GH50) first two panels on overview page

---
+++ brand-themes/bbva/overview.js
@@ -0,0 +1,47 @@
+module.exports = {
+	panels: {
+		audience_population: {
+			title: 'Audience Population',
+			img: '/images/slices/index.audience-segments-population.svg',
+			tabs: [
+				{ label: 'This Month (Aug 1 &mdash; Aug 7, 2018)' },
+				{ label: 'Last Month (Jul 1 &mdash; Jul 31, 2018)' }
+			]
+		},
+		activated_segments : {
+			buttons: [
+				{ label: 'Top 5' },
+				{ label: 'By Impressions' },
+				{ label: 'Last 30 days' }
+			],
+			column_headers: [
+				'SEGMENTS',
+				'IMPRESSIONS'
+			],
+			segments: [
+				{label: '<img src="/images/icons/dollar.svg" /> Gold Customers_2018_High propensity shoppers', number: '5.2M', width: 362},
+				{label: 'Loyal_Shoppers_2016-2017_potential target', number: '4.7M', width: 308},
+				{label: '<img src="/images/icons/dollar.svg" /> Heavy spenders', number: '4M', width: 290},
+				{label: 'Deals_clicks_converts_group_deal-shoppers_2018', number: '3.7M', width: 270},
+				{label: 'Early_adopters', number: '2.3M', width: 186}
+			],
+			title: 'Activated Segments',
+			view_all: {
+				text: 'View All Segments',
+				href: 'javascript:void(0);'
+			}
+		},
+		dce : {
+			title: 'Data Capture Events (DCE)',
+		},
+		dcm : {
+			title: 'Data Capture Methods',
+		},
+		marketer_campaigns : {
+			title: 'Marketer Campaigns',
+		},
+		activation_partners : {
+			title: 'Activation Partners',
+		}
+	}
+}
