62579a17b8d282b419173e8d5da7b5dcf70e0a53
Christopher McCulloh <cmcculloh@salesforce.com>
2018-08-30 16:43:02 -0400

    (GH52) localizes consumer rights management page

---
+++ brand-themes/bbva/consumer-rights-management.js
@@ -0,0 +1,34 @@
+module.exports = {
+	consent_distribution: [
+		{label: 'Data Collection', width: 460},
+		{label: 'Targeting', width: 339},
+		{label: 'Analytics', width: 106},
+		{label: 'Data Sharing', width: 324},
+		{label: 'Data Selling', width: 85},
+		{label: 'Re-Identification', width: 92}
+	],
+	consent_sources: [
+		{
+			name: 'Policy Regime Consent',
+			y: 75,
+			color: '#73ACE8'
+		},
+		{
+			name: 'Backfill Consent',
+			y: 10,
+			color: '#FFD33F'
+		},
+		{
+			name: '1st Party Consent',
+			y: 10,
+			color: '#F49E60'
+		},
+		{
+			name: '2nd Party Consent',
+			y: 5,
+			color: '#EE7C8F'
+		}
+	],
+	last_updated: 'Jun 12, 2018',
+	sub_header_text: 'As of Jun 13, 2018',
+};
