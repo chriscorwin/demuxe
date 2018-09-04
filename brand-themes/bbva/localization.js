440fc7e9552fc8dae8b78a1099bf845b7e605f3d
Christopher Corwin <ccorwin@ccorwin-ltm2.internal.salesforce.com>
2018-09-04 17:48:19 -0400

    WIP

--- brand-themes/bbva/localization.js
+++ brand-themes/bbva/localization.js
@@ -1,8 +1,10 @@
 const segments = require('./segments');
 const overview = require('./overview');
+const magicFlowsMetaData = require('./magic-flows-meta-data');
 
 module.exports = {
 	"NAME": "BBVA",
+	magicFlowsMetaData,
 	segments,
 	overview
 };
---
+++ brand-themes/bbva/magic-flows-meta-data.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"bbva-mobile": {
+		"customContentStyles": {
+			"0": {"margin-top": "93px","margin-bottom": "84px"},
+			"1": {"margin-top": "137px","margin-bottom": "155px"},
+			"2": {"margin-top": "137px","margin-bottom": "77px"},
+			"3": {"margin-top": "137px","margin-bottom": "155px"},
+			"4": {"margin-top": "137px","margin-bottom": "155px"},
+			"5": {"margin-top": "137px","margin-bottom": "65px"},
+			"20": {"margin-top": "80px","margin-bottom": "0"},
+			"21": {"margin-top": "80px","margin-bottom": "0"},
+			"22": {"margin-top": "80px","margin-bottom": "0"},
+			"23": {"margin-top": "0","margin-bottom": "0"},
+			"24": {"margin-top": "80px","margin-bottom": "0"},
+			"25": {"margin-top": "80px","margin-bottom": "0"},
+			"26": {"margin-top": "80px","margin-bottom": "0"},
+		}
+	}
+};
