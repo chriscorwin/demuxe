2700e95b78f5690be0021146c0373afc2f19d8e6
Christopher Corwin <ccorwin@ccorwin-ltm2.internal.salesforce.com>
2018-09-04 11:43:07 -0400

    WIP

--- product-templates/mobile/javascripts/flow.js
+++ product-templates/mobile/javascripts/flow.js
@@ -2,7 +2,11 @@ console.log(`/product-templates/dmp/javascripts/flow.js running`);
 
 const magickFlowConfig = locals.demoMagickFlows[demoMagickFlowDirectoryName];
 
-let clicks = parseInt( window.location.hash.replace( '#', '' ) ) || 0;
+// let clicks = parseInt( window.location.hash.replace( '#', '' ) ) || 0;
+let clicks = parseInt( locals.sanitizedQueryParams.screen ) || 0;
+
+console.log(`locals.sanitizedQueryParams.screen: `, locals.sanitizedQueryParams.screen);
+
 
 window.location.hash = `#${clicks}`;
 
@@ -10,12 +14,13 @@ window.location.hash = `#${clicks}`;
 const $ss = document.querySelector( '#screenshot' );
 const $contentWrapper = document.querySelector( '#content-wrapper' );
 
-$ss.onclick = ( ) => {
+$contentWrapper.onclick = ( ) => {
 	clicks++;
 	if ( clicks >= magickFlowConfig.numberOfScreens ) {
 		clicks = 0;
 	}
 	window.location.hash = `#${clicks}`;
+	window.location.search = `screen=${clicks}`;
 };
 
 
@@ -23,6 +28,7 @@ function locationHashChanged( ) {
 	console.log(`clicks: `, clicks);
 	clicks = parseInt( window.location.hash.replace( '#', '' ) ) || 0;
 	console.log(`clicks >= magickFlowConfig.numberOfScreens: `, clicks >= magickFlowConfig.numberOfScreens);
+	window.location.search = `screen=${clicks}`;
 	if ( clicks >= magickFlowConfig.numberOfScreens ) {
 		clicks = 0;
 		window.location.hash = `#${clicks}`;
