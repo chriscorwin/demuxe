89e5dfd51265fbb2226c4b2dfb1ffb23f75b400a
Christopher McCulloh <cmcculloh@salesforce.com>
2018-09-18 09:20:44 -0400

    (SPENCER) WIP on spencer's stuff

---
+++ brand-themes/nto/overview.js
@@ -0,0 +1,200 @@
+module.exports = {
+	panels: {
+		audience_population: {
+			title: 'Audience Population',
+			img: '/images/slices/index.audience-segments-population.svg',
+			tabs: [
+				{ label: 'This Month (Sep 1 &mdash; Sep 23, 2018)' },
+				{ label: 'Last Month (Aug 1 &mdash; Aug 31, 2018)' }
+			]
+		},
+		activated_segments : {
+			buttons: [
+				{ label: 'Top 5' },
+				{ label: 'By Impressions' },
+				{ label: 'Last 30 days' }
+			],
+			column_headers: [
+				'SEGMENTS',
+				'IMPRESSIONS'
+			],
+			segments: [
+				{label: 'Mortgage Applicants 2018', number: '5.2M', width: 362},
+				{label: 'Corporate Customers - Risk Management', number: '4.7M', width: 308},
+				{label: 'Loan Expert Seekers', number: '4M', width: 290},
+				{label: 'Mortgage Aspirants', number: '3.7M', width: 270},
+				{label: 'Credit Card Members - Optimizer Card', number: '2.3M', width: 186}
+			],
+			title: 'Activated Segments',
+			view_all: {
+				text: 'View All Segments',
+				href: 'javascript:void(0);'
+			}
+		},
+		dce : {
+			buttons: [
+				{ label: 'Last 30 days' }
+			],
+			explanation: 'Displaying data collection events across 1st party data sources only.',
+			graph: {
+				date_labels: [
+					'7/13',
+					'7/27',
+					'8/05'
+				],
+				device_data: [
+					450,
+					490,
+					490,
+					590,
+					590,
+					610,
+					660,
+					730,
+					660,
+					655,
+					650,
+					730,
+					740,
+					739,
+					780,
+					830,
+					890,
+					920,
+					910,
+					880,
+					890,
+					880,
+					860,
+					840,
+					820,
+					860,
+					880,
+					890,
+					920,
+					910
+				],
+				// Do not replace `function formatter () {` with `() => {}` as that will change the context of `this`
+				label_formatter: function formatter () {
+					if (this.value === 400000) {
+						return '0K';
+					}
+					if (this.value < 1000000) {
+						return (this.value / 1000) + 'K';
+					}
+					return (this.value / 1000000) + 'M';
+				},
+				line_color: '#9961B4',
+				title: 'Data Capture Events (DCE)'
+			},
+			title: 'Data Capture Events (DCE)',
+			view_all: {
+				text: 'View All Data Capture Events',
+				href: 'javascript:void(0);'
+			}
+		},
+		dcm : {
+			buttons: [
+				{ label: 'By Data Capture Events' },
+				{ label: 'Last 30 days' }
+			],
+			graph: {
+				methods: [
+					{
+						name: 'Web',
+						y: 100,
+						color: '#70ABEA'
+					},
+					{
+						name: 'App',
+						y: 85,
+						color: '#EDBE32'
+					},
+					{
+						name: 'Offline Files',
+						y: 50,
+						color: '#F2536D'
+					},
+					{
+						name: 'Partners',
+						y: 45,
+						color: '#7DC37D'
+					}
+				]
+			},
+			title: 'Data Capture Sources',
+			view_all_buttons: [
+				{
+					id: 'view-all-data-capture-sources',
+					text: 'View All Data Capture Sources',
+					href: 'javascript:void(0);',
+					onclick: "navigatePage('/manage/data-capture/sources');"
+				}
+			]
+		},
+		marketer_campaigns: {
+			buttons: [
+				{ label: 'Top 5' },
+				{ label: 'By Impressions' },
+				{ label: 'Last 30 days' }
+			],
+			graph: {
+				CAMPAIGN: 'campaign',
+				spend_data: [
+					{ x: 240, y: 2250, z: 77, name: 'Display', color: '#0857A6' },
+					{ x: 370, y: 1400, z: 48, name: 'Email', color: '#5208A6' }, // Email on left
+					{ x: 810, y: 870, z: 59, name: 'Mobile', color: '#197EE3' },
+					{ x: 560, y: 1900, z: 27, name: 'Email', color: '#7719E3' }, // Email on right
+					{ x: 990, y: 2250, z: 56, name: 'Video', color: '#C398F5' }
+				],
+				tooltip: '<div style="width: 70px;"><div style="background-color: {point.color};height: 10px; width: 10px;border-radius: 5px;float: left;margin: 10px 10px 10px 4px;"></div> {point.name}<br />{point.y}K</div>',
+				x_axis: {
+					// Do not replace `function formatter () {` with `() => {}` as that will change the context of `this`
+					formatter: function formatter () {
+						if (this.value < 1000) {
+							return this.value + 'K';
+						}
+						return (this.value / 1000) + 'M';
+					},
+					title: 'Impressions'
+				},
+				y_axis: {
+					// Do not replace `function formatter () {` with `() => {}` as that will change the context of `this`
+					formatter: function formatter () {
+						if (this.value < 1000) {
+							return this.value + 'K';
+						}
+						return (this.value / 1000) + 'M';
+					},
+					line_color: '#E6EBF5',
+					title: 'Spend ($)'
+				}
+			},
+			title: 'Marketer Campaigns',
+			view_all: {
+				text: 'View All Marketer Campaigns',
+				href: 'javascript:void(0);'
+			}
+		},
+		activation_partners : {
+			buttons: [
+				{ label: 'Top 5' },
+				{ label: 'By Devices Sent' },
+				{ label: 'Aug 5, 2018' }
+			],
+			table_rows: [
+				{partner: 'ACTIVATION PARTNERS', type: 'REFRESH TYPE', sent: 'DEVICES SENT'},
+				{partner: '<img src="/images/logos/logo-google-ads.svg" />', type: 'Full', sent: '5M'},
+				{partner: '<img src="/images/logos/sf.svg" />', type: 'Full', sent: '3.1M'},
+				{partner: '<img src="/images/logos/appnexus.svg" />', type: 'Incremental', sent: '2.8M'},
+				{partner: '<img src="/images/logos/youtube.svg" />', type: 'Incremental', sent: '2.5M'},
+				{partner: '<img src="/images/logos/pinterest.svg" />', type: 'Full', sent: '1.8M'}
+			],
+			title: 'Activation Partners',
+			view_all: {
+				text: 'View All Activation Partners',
+				href: 'javascript:void(0);'
+			}
+		}
+	}
+};
---
+++ brand-themes/nto/segments.js
@@ -0,0 +1,67 @@
+module.exports = {
+	default: [
+		{
+			title: 'Loyalty Low Rating',
+			subTitle: 'Standard Segment / ID: <b>rsdqtbhe8</b> <img src="/images/icons/sort.svg" />',
+			devices: '58.41M',
+			audience: '25.17M',
+			modified: 'Aug 19, 2018',
+			type: 'Tablet Insurance',
+			category: '',
+			subCategory: 'Loyalty'
+		},
+		{
+			title: 'Lonely Planet Adventurer Traveler',
+			subTitle: 'Standard Segment / ID: <b>rsdqtbya</b>',
+			devices: '91.3M',
+			audience: '31.1M',
+			modified: 'Aug 19, 2018',
+			type: 'Demographic',
+			category: 'Demographic',
+			subCategory: 'Gender'
+		},
+		{
+			title: 'JetBlue SLC, Denver, Jackson Hole Fliers',
+			subTitle: 'Standard Segment / ID: <b>rsdqtah9w</b>',
+			devices: '70.4M',
+			audience: '24M',
+			modified: 'Aug 19, 2018',
+			type: 'Demographic',
+			category: 'Demographic',
+			subCategory: 'Age'
+		},
+		{
+			title: 'Site Engaged - Hiking Customers',
+			subTitle: 'Standard Segment / ID: <b>rsdqtbasi</b>',
+			devices: '45.3M',
+			audience: '14.2M',
+			modified: 'Feb 27, 2018',
+			type: 'Clusters',
+			category: 'Behavioural',
+			subCategory: 'Attributes'
+		},
+		{
+			title: 'Site Engaged - Hiking Customers',
+			subTitle: 'Standard Segment / ID: <b>rsdqtbasi</b>',
+			devices: '45.3M',
+			audience: '14.2M',
+			modified: 'Feb 27, 2018',
+			type: 'Clusters',
+			category: 'Behavioural',
+			subCategory: 'Attributes'
+		}
+	],
+	// These will be added in reverse order listed here
+	additional: [
+		{
+			title: '<span class="slds-badge slds-m-right_small">LOOKALIKE</span><img src="/images/logo-noname.svg" height=10 /> High Value Customers Lookalikes',
+			subTitle: 'Standard Segment / ID: <b>rsdqtbhe8</b> <img src="/images/icons/sort.svg" />',
+			devices: '13.8M',
+			audience: '2.8M',
+			modified: 'August 06, 2018',
+			type: 'Standard',
+			category: 'Behavioral',
+			subCategory: 'Loyalty'
+		}
+	]
+};
