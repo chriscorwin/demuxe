845609ae2a94895bb579813229f48fcbc2b7dd8b
Christopher McCulloh <cmcculloh@salesforce.com>
2019-01-25 17:15:47 -0500

    (5632143) inserts rapid div into page when button clicked

--- engine/includes/rpd/controller.js
+++ engine/includes/rpd/controller.js
@@ -1,44 +1,23 @@
-const showHideRPDController = () => {
-	const rpdController = document.getElementById('RPDController');
-
-	if (rpdController.style.display === 'none') {
-		rpdController.style.display = 'block';
+const showHide = (target) => {
+	if (target.style.display === 'none') {
+		target.style.display = 'block';
 	} else {
-		rpdController.style.display = 'none'
+		target.style.display = 'none'
 	}
 }
 
-const toggleRPDController = () => {
-	const sectionStyles = document.querySelector('#RPDController > div > section').classList;
-
-	if (sectionStyles.contains('slds-is-open')) {
-		sectionStyles.remove('slds-is-open');
-		sectionStyles.add('slds-is-closed');
+const toggleOpen = (classList) => {
+	if (classList.contains('slds-is-open')) {
+		classList.remove('slds-is-open');
+		classList.add('slds-is-closed');
 	} else {
-		sectionStyles.add('slds-is-open');
-		sectionStyles.remove('slds-is-closed');
+		classList.add('slds-is-open');
+		classList.remove('slds-is-closed');
 	}
 }
 
-const addListener = () => {
-	console.log('KONAMI CODE LISTENER ACTIVE. To deactivate, add ?disableKonami=true to URL. UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A to show/hide RPD Controller');
-	let cursor = 0;
-	const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-	document.addEventListener('keydown', (e) => {
-		cursor = (e.keyCode == KONAMI_CODE[cursor]) ? cursor + 1 : 0;
-		if (cursor == KONAMI_CODE.length) {
-			showHideRPDController();
-			cursor = 0;
-		};
-	});
-}
-
-document.querySelector('#closeRPDController').addEventListener('click', showHideRPDController);
-document.querySelector('#minimizeRPDController').addEventListener('click', toggleRPDController);
-
-const addRapidDiv = () => {
-	console.log('add rapid div');
+const addRapidDiv = (target) => {
+	target.insertAdjacentHTML('beforeend', `<div class="rapidDiv">This is a rapid div</div>`);
 }
 
 
-document.querySelector('#addRapidDiv').addEventListener('click', addRapidDiv);
