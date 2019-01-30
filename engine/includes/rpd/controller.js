14893e4c6c8272024372feaf77e99255ccd47a38
Christopher McCulloh <cmcculloh@salesforce.com>
2019-01-30 13:22:25 -0500

    (5632143) makes rpdDiv re-positionable via drag/drop

--- engine/includes/rpd/controller.js
+++ engine/includes/rpd/controller.js
@@ -1,8 +1,20 @@
-const showHide = (target) => {
+const enableEditMode = (elm) => {
+	elm.classList.add('editMode');
+}
+
+const disableEditMode = (elm) => {
+	elm.classList.remove('editMode');
+}
+
+const showHide = (target, parent) => {
 	if (target.style.display === 'none') {
 		target.style.display = 'block';
+
+		enableEditMode(parent);
 	} else {
 		target.style.display = 'none'
+		
+		disableEditMode(parent);
 	}
 }
 
@@ -16,8 +28,64 @@ const toggleOpen = (classList) => {
 	}
 }
 
+const editModeEnabled = () => {
+
+}
+
+const updateOffsets = (rpdDiv, e) => {
+	return {
+		divLeft: rpdDiv.offsetLeft,
+		divTop: rpdDiv.offsetTop,
+		mouseLeft: e.clientX,
+		mouseTop: e.clientY,
+		left: e.clientX - rpdDiv.offsetLeft,
+		top: e.clientY - rpdDiv.offsetTop
+	};
+}
+
+const addListeners = (rpdDiv) => {
+	let offsets;
+
+	const handleMove = (e) => {
+		e.preventDefault();
+		
+		const mouseTop = e.clientY;
+		const mouseLeft = e.clientX;
+		rpdDiv.style.top = `${mouseTop - offsets.top}px`;
+		rpdDiv.style.left = `${mouseLeft - offsets.left}px`;
+
+		offsets = updateOffsets(rpdDiv, e);
+	}
+
+	rpdDiv.addEventListener('mousedown', (e) => {
+		rpdDiv.classList.add('grabbing');
+
+		offsets = updateOffsets(rpdDiv, e);
+
+		rpdDiv.addEventListener('mousemove', handleMove);
+	});
+	rpdDiv.addEventListener('mouseup', (e) => {
+		rpdDiv.classList.remove('grabbing');
+
+		offsets = {};
+
+		rpdDiv.removeEventListener('mousemove', handleMove);
+	});
+
+}
+
 const addRapidDiv = (target) => {
-	target.insertAdjacentHTML('beforeend', `<div class="rapidDiv">This is a rapid div</div>`);
+	const uniqueID = `rpdDiv${Date.now()}`;
+	const rapidDiv = `
+		<div id="${uniqueID}" class="rapidDiv">
+			Rapid Div ${uniqueID}
+		</div>
+`;
+
+	target.insertAdjacentHTML('beforeend', rapidDiv);
+
+	const rpdDiv = target.querySelector(`#${uniqueID}`);
+	addListeners(rpdDiv);
 }
 
 
