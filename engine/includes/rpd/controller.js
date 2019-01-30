0c7ec3007de933e945da927afeaae764d44fe07c
Christopher McCulloh <cmcculloh@salesforce.com>
2019-01-30 15:50:20 -0500

    (5632143) adds clear selection button. WIP making RPD Divs selectable

--- engine/includes/rpd/controller.js
+++ engine/includes/rpd/controller.js
@@ -32,6 +32,51 @@ const editModeEnabled = () => {
 
 }
 
+const showHomeForm = (RPDController) => {
+	RPDController.querySelector('.slds-docked-composer__body_form').innerHTML = `
+<fieldset class="slds-form-element slds-form_compound">
+	<div class="slds-form-element__control">
+		<div class="slds-form-element__group slds-grid slds-wrap">
+			<ul class="slds-button-group-list slds-col slds-size_1-of-1">
+				<li>
+					<button id="addRapidDiv" class="slds-button slds-button_neutral"><svg class="slds-button__icon slds-button__icon_small slds-button__icon_left" aria-hidden="true">
+						<use xlink:href="/icons/utility-sprite/svg/symbols.svg#steps"></use>
+					</svg>Add a Rapid Div</button>
+				</li>
+			</ul>
+		</div>
+	</div>
+</fieldset>
+`;
+	
+	RPDController.querySelector('.slds-docked-composer__footer').classList.add('slds-hidden');
+}
+
+const setSelection = (rpdDiv, RPDController) => {
+	RPDController.querySelector('.slds-docked-composer__body_form').innerHTML = `
+<fieldset class="slds-form-element slds-form_compound">
+	<div class="slds-form-element__control">
+		<div class="slds-form-element__group slds-grid slds-wrap">
+			<ul class="slds-button-group-list slds-col slds-size_1-of-1 slds-m-left_none slds-m-top_xx-small">
+				<li>
+					<button class="slds-button slds-button_neutral"><svg class="slds-button__icon slds-button__icon_small slds-button__icon_left" aria-hidden="true">
+						<use xlink:href="/icons/utility-sprite/svg/symbols.svg#cases"></use>
+					</svg>View Selection Code</button>
+				</li>
+				<li>
+					<button class="slds-button slds-button_neutral"><svg class="slds-button__icon slds-button__icon_small slds-button__icon_left" aria-hidden="true">
+						<use xlink:href="/icons/utility-sprite/svg/symbols.svg#copy_to_clipboard"></use>
+					</svg>Copy Selection Code</button>
+				</li>
+			</ul>
+		</div>
+	</div>
+</fieldset>
+	`;
+
+	RPDController.querySelector('.slds-docked-composer__footer').classList.remove('slds-hidden');
+}
+
 const updateOffsets = (rpdDiv, e) => {
 	return {
 		divLeft: rpdDiv.offsetLeft,
@@ -43,12 +88,12 @@ const updateOffsets = (rpdDiv, e) => {
 	};
 }
 
-const addListeners = (rpdDiv) => {
+const addListeners = (rpdDiv, RPDController) => {
 	let offsets;
 
 	const handleMove = (e) => {
 		e.preventDefault();
-		
+
 		const mouseTop = e.clientY;
 		const mouseLeft = e.clientX;
 		rpdDiv.style.top = `${mouseTop - offsets.top}px`;
@@ -58,6 +103,8 @@ const addListeners = (rpdDiv) => {
 	}
 
 	rpdDiv.addEventListener('mousedown', (e) => {
+		setSelection(rpdDiv, RPDController);
+
 		rpdDiv.classList.add('grabbing');
 
 		offsets = updateOffsets(rpdDiv, e);
@@ -74,7 +121,7 @@ const addListeners = (rpdDiv) => {
 
 }
 
-const addRapidDiv = (target) => {
+const addRapidDiv = (target, RPDController) => {
 	const uniqueID = `rpdDiv${Date.now()}`;
 	const rapidDiv = `
 		<div id="${uniqueID}" class="rapidDiv">
@@ -85,7 +132,7 @@ const addRapidDiv = (target) => {
 	target.insertAdjacentHTML('beforeend', rapidDiv);
 
 	const rpdDiv = target.querySelector(`#${uniqueID}`);
-	addListeners(rpdDiv);
+	addListeners(rpdDiv, RPDController);
 }
 
 
