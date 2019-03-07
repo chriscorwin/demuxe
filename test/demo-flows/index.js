72af1a046f9340623235d3cdc91ee58094a19a42
Christopher McCulloh <cmcculloh@salesforce.com>
2019-03-07 15:56:15 -0500

    (W-59067) test run flow

--- test/demo-flows/index.js
+++ test/demo-flows/index.js
@@ -1,6 +1,6 @@
 const dmp_df18keynote_bbva = require('./dmp_df18keynote_bbva');
 const dmp_df18keynote_crocs = require('./dmp_df18keynote_crocs');
-const jb_test_run_nto = require('./jb_test_run_nto');
+const jb_test_run_nto = require('./jb_test-run_nto');
 
 module.exports = [
 	dmp_df18keynote_bbva,
