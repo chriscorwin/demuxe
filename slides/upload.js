b30ac8cdc6a3cc3468fb1f8ba062005444124bcb
Christopher McCulloh <cmcculloh@salesforce.com>
2019-04-03 16:31:02 -0400

    generate slides with npm command

---
+++ slides/upload.js
@@ -0,0 +1,21 @@
+const config = require('../config/config.json');
+
+const { exec } = require('child_process');
+
+const slides_id = config.presentationid ? `-a ${config.presentationid} -e` : '';
+
+console.log(`md2gslides slides/dmp.roadshow19.nto.md ${slides_id}`);
+
+exec(`md2gslides slides/dmp.roadshow19.nto.md ${slides_id}`, (err, stdout, stderr) => {
+	if (err) {
+		console.error(`error ${err}`);
+		return;
+	}
+
+	if (stderr) {
+		console.warn(`stderr: ${stderr}`);
+	}
+
+	console.log(`stdout: ${stdout}`);
+});
+
