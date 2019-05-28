0f7d1ddddd90b035e9f3ef308847beecc320c342
Christopher McCulloh <cmcculloh@salesforce.com>
2019-05-28 16:49:33 -0400

    copy/paste npm command to set your config

--- config/config.sh
+++ config/config.sh
@@ -1,2 +1,6 @@
 rm config/config.json
 cp config/config.$1.$2.$3.json config/config.json
+
+# quick copy/paste to set your config:
+# npm run set-config audience-studio cnx19keynote fever
+# npm run set-config audience-360 cnx19keynote fever
