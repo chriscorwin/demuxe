76589c10068ad107bcc9dc2fe5bb6884501eb9ef
Christopher McCulloh <cmcculloh@salesforce.com>
2019-05-31 13:49:07 -0400

    lookalikes JSON

---
+++ demo-overrides/audience-studio/cnx19keynote/lookalikes.js
@@ -0,0 +1,58 @@
+const lookalikes = {
+	tableData: {
+		rows: [
+			{
+				reach: '2,879,152', similarity: '78%'
+			},
+			{
+				reach: '3,283,156', similarity: '72%'
+			},
+			{
+				reach: '4,283,180', similarity: '65%'
+			},
+			{
+				reach: '4,721,100', similarity: '60%'
+			},
+			{
+				reach: '4,803,480', similarity: '58%'
+			},
+			{
+				reach: '5,087,930', similarity: '52%'
+			},
+			{
+				reach: '6,237,498', similarity: '50%'
+			},
+			{
+				reach: '7,283,180', similarity: '47%'
+			},
+			{
+				reach: '8,293,582', similarity: '40%'
+			},
+			{
+				reach: '10,874,201', similarity: '37%'
+			},
+			{
+				reach: '11,453,000', similarity: '34%'
+			}
+		],
+		labels: ['Similarity', 'Reach']
+	}
+};
+
+module.exports = {
+	segment: {
+		title: 'Family Prospect Targets',
+		subTitle: 'Standard Segment / ID: <b>rsdqtbhe8</b> <img src="/images/icons/sort.svg" />',
+		subTitleBadges: [{ text: 'Daily Refresh', icon: { placement: 'left', src: '/icons/action-sprite/svg/symbols.svg#refresh' }}],
+		devices: '6.3M',
+		audience: '5M',
+		audienceFill: 80,
+		modified: aboutOneMonthAgo,
+		type: 'Standard',
+		category: 'Behavioral',
+		subCategory: 'Loyalty'
+	},
+	lookalikes
+};
+
+4,283,180
