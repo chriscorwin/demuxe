6ff6a29d115aa7c5ef31ceac86e0662656ced0e2
Christopher McCulloh <cmcculloh@salesforce.com>
2019-07-19 14:58:39 -0400

    display thumbnails of screenshots along with step taken in demo to generate screenshot

--- config/available-demos.js
+++ config/available-demos.js
@@ -11,6 +11,17 @@ const addAvailableDemoAppConfigs = (configData) => {
 
 	require("fs").readdirSync(demosConfigPath).forEach(function(file) {
 		const config = require(`./demos/${file}`);
+
+		const screenshotsPath = require("path").join(__dirname, "../slides/screenshots");
+		const screenshotMatch = `${config.productTemplate}.${config.demoVenue}.${config.brandTheme}`;
+		config.availableScreenshots = require("fs").readdirSync(screenshotsPath).filter((file) => file.match(screenshotMatch));
+
+		try{
+			config.demoFlow = require(`../test/demo-flows/${config.productTemplate}_${config.demoVenue}_${config.brandTheme}`);
+		} catch(err){
+			config.demoFlow = [];
+		}
+
 		availableDemoConfigs.push(config);
 	});
 
