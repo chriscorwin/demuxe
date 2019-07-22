b501a240a89dab91dfb8d98ff2ae388b3afb74d7
Christopher McCulloh <cmcculloh@salesforce.com>
2019-07-22 12:53:02 -0400

    demo groupings data loading in and showing on bottom of overview page

--- product-templates/catalogue/default-config.js
+++ product-templates/catalogue/default-config.js
@@ -1,18 +1,24 @@
-const demosGroupsConfigPath = require("path").join(__dirname, "demo_groups");
-const demoGroups = [];
+const demosGroupsConfigPath = require("path").join(__dirname, "demo_groupings");
+let demoGroups = [];
 
 require("fs").readdirSync(demosGroupsConfigPath).forEach(function(file) {
-	const config = require(`./demo_groups/${file}`);
+	try {
+		const config = require(`./demo_groupings/${file}`);
 
-	demoGroups.push(config);
+		demoGroups.push(config);
+	} catch (e) {
+		console.error('error pulling in demo groupings', e);
+	}
 });
 
+// Todo: pull in dynDate branch to get MomentJS stuff to actually compare these as dates
+demoGroups.sort((groupA, groupB) => groupA.date < groupB.date)
 
 module.exports = {
 	account_name: '',
 	appName: 'Prototype Library',
-    body_class: 'slds-brand-band slds-brand-band_medium',
-    header: {
+	body_class: 'slds-brand-band slds-brand-band_medium',
+	header: {
 		favorites: { disabled: true },
 		global_actions: false,
 		help: false,
