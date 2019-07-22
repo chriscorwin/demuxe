a35f2220e99aa32fe4677d440cb7c9c12c018a12
Christopher McCulloh <cmcculloh@salesforce.com>
2019-07-22 12:31:32 -0400

    data structure for demo groupings

--- product-templates/catalogue/default-config.js
+++ product-templates/catalogue/default-config.js
@@ -1,13 +1,23 @@
+const demosGroupsConfigPath = require("path").join(__dirname, "demo_groups");
+const demoGroups = [];
+
+require("fs").readdirSync(demosGroupsConfigPath).forEach(function(file) {
+	const config = require(`./demo_groups/${file}`);
+
+	demoGroups.push(config);
+});
+
+
 module.exports = {
 	account_name: '',
 	appName: 'Prototype Library',
     body_class: 'slds-brand-band slds-brand-band_medium',
     header: {
-		favorites: { disabled: false },
+		favorites: { disabled: true },
 		global_actions: false,
-		help: true,
-		setup: true,
-		notifications: true,
+		help: false,
+		setup: false,
+		notifications: false,
 		search: true,
 		hideSubheader: false,
 		// avatar: `/images/icons/Identity.svg`
@@ -15,5 +25,6 @@ module.exports = {
 	navData: {
 		accountNavItems: [],
 		navItems: []
-	}
+	},
+	demoGroups
 };
