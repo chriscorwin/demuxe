2d8d4d427549f48a0869b13a9078f0186ba759e4
Christopher McCulloh <cmcculloh@salesforce.com>
2019-08-14 16:52:12 -0400

    start embedded videos at the correct point. Give better defaults for poorly defined magick flows

--- test/demo-flows/audience-studio_cnx19keynote_fever.js
+++ test/demo-flows/audience-studio_cnx19keynote_fever.js
@@ -18,41 +18,48 @@ module.exports = {
 		{
 			goto: testhost + '',
 			waitFor: 'body',
-			name: '0001.manage-segments'
+			name: '0001.manage-segments',
+			description: 'go to manage segments'
 		},
 		{
 			evaluate: () => {
 				window.scrollBy(0, window.innerHeight);
 			},
 			waitFor: 2000,
-			name: '0002.manage-segments'
+			name: '0002.manage-segments',
+			description: 'scroll down'
 		},
 		{
 			click: '.slds-panel-box__header-settings',
 			waitFor: 500,
-			name: '0010.manage-segments.click-settings'
+			name: '0010.manage-segments.click-settings',
+			description: 'click settings cog wheel in first row of table'
 		},
 		{
 			click: '.slds-panel-box .slds-dropdown__item:nth-of-type(4)',
 			waitFor: 2000,
-			name: '0020.lookalikes'
+			name: '0020.lookalikes',
+			description: 'click lookalikes'
 		},
 		{
 			evaluate: () => {
 				window.scrollBy(0, window.innerHeight);
 			},
 			waitFor: 2000,
-			name: '0020.lookalikes.scrolled'
+			name: '0020.lookalikes.scrolled',
+			description: 'scroll down and select a lookalike by clicking one of the small dots'
 		},
 		{
 			goto: testhost + '?state=addLookalike',
 			waitFor: 2000,
-			name: '0030.manage-segments.lookalike-added'
+			name: '0030.manage-segments.lookalike-added',
+			description: 'click "Create Lookalike Segment" button'
 		},
 		{
 			click: '#einsteinBadge',
 			waitFor: 200,
-			name: '0035.manage-segments.einstein-clicked'
+			name: '0035.manage-segments.einstein-clicked',
+			description: "click on Einstein's head in the first row in the table"
 		}
 	]
 };
