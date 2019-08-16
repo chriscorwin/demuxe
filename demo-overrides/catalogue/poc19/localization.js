fb3565728be7e6263fd57ba0aab7d0edecd5507c
Christopher McCulloh <cmcculloh@salesforce.com>
2019-08-16 15:48:55 -0400

    graph specific localization/overrides

--- demo-overrides/catalogue/poc19/localization.js
+++ demo-overrides/catalogue/poc19/localization.js
@@ -1,12 +1,22 @@
 module.exports = {
 	body_class: ' yer-mom-wears-body_class',
-	"stop-1": "10",
-	"stop-2": "30",
-	"stop-3": "40",
-	"stop-4": "70",
-	"stop-5": "110",
-	"stop-6": "180",
-	"stop-7": "290",
-	"label-4": "POC",
-	"label-5": "Replaced"
+	plain: {
+		"title": "new title",
+		"stop-1": "10",
+		"stop-2": "30",
+		"stop-3": "40",
+		"stop-4": "70",
+		"stop-5": "110",
+		"stop-6": "180",
+		"stop-7": "290",
+		"label-1": "POC",
+		"label-2": "Replaced"
+	},
+	"filled-in": {
+		"title":  "Last 90 Days",
+		"stop-1": "10%",
+		"stop-3": "15%",
+		"stop-5": "20%",
+		"stop-7": "25%"
+	}
 };
