99c1e823d00b8ede2098566ef5334df4a8e1eefa
Christopher McCulloh <cmcculloh@salesforce.com>
2019-08-16 17:23:51 -0400

    cleaning up the bar graph and making more options

--- demo-overrides/catalogue/poc19/localization.js
+++ demo-overrides/catalogue/poc19/localization.js
@@ -2,21 +2,43 @@ module.exports = {
 	body_class: ' yer-mom-wears-body_class',
 	plain: {
 		"title": "new title",
-		"stop-1": "10",
-		"stop-2": "30",
-		"stop-3": "40",
-		"stop-4": "70",
-		"stop-5": "110",
-		"stop-6": "180",
-		"stop-7": "290",
+		"stop-0": "",
+		"stop-25": "10%",
+		"stop-50": "15%",
+		"stop-75": "20%",
+		"stop-100": "25%",
 		"label-1": "POC",
 		"label-2": "Replaced"
 	},
 	"filled-in": {
 		"title":  "Last 90 Days",
-		"stop-1": "10%",
-		"stop-3": "15%",
-		"stop-5": "20%",
-		"stop-7": "25%"
+		"stop-0": "",
+		"stop-25": "10%",
+		"stop-50": "15%",
+		"stop-75": "20%",
+		"stop-100": "25%"
+	},
+	"graph-1": {
+		"title": "This is  Graph 1",
+		"stop-0": "",
+		"stop-20": "20%",
+		"stop-40": "40%",
+		"stop-60": "60%",
+		"stop-80": "80%",
+		"stop-100": "100%",
+		"label-1": "Full Agreement",
+		"label-2": "Partial Agreement",
+		"label-3": "Neutral",
+		"label-4": "Partial Disagreement",
+		"label-5": "Full Disagreement"
+	}
+	,
+	"graph-2": {
+		"title": "This is  Graph 2",
+		"stop-0": "",
+		"stop-25": "10%",
+		"stop-50": "15%",
+		"stop-75": "20%",
+		"stop-100": "25%",
 	}
 };
