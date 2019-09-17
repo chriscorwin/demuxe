c5fe23a63b242b4654fd71cbe82c2ea23624700c
Christopher McCulloh <cmcculloh@salesforce.com>
2019-09-17 16:12:19 -0400

    lineheight cleanup in place

--- config/clean-svgs.js
+++ config/clean-svgs.js
@@ -6,6 +6,8 @@ Demuxe: Running \`config/clean-svgs.js\` now...
 const path = require('path');
 const util = require('util');
 const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const { execSync } = require('child_process');
 
@@ -27,9 +29,33 @@ const cleanSVGs = (configData) => {
 		execSync(`mkdir -p ${fileOrDirectoryPath}/main-src-originals && mv ${fileOrDirectoryPath}/main-src/* ${fileOrDirectoryPath}/main-src-originals/`);
 
 		// do lineheight stuff
+		const subDirectoryContents = fs.readdirSync(path.join(fileOrDirectoryPath, 'main-svgo-processed'));
 
+		subDirectoryContents.forEach(subFileOrDirectory => {
+			if (subFileOrDirectory === '.DS_Store') {
+				console.log('is DS_Store. Skip');
+				return;
+			}
+			console.log(`considering file: ${subFileOrDirectory}`);
+			const fileContents = fs.readFileSync(path.join(fileOrDirectoryPath, 'main-svgo-processed', subFileOrDirectory), 'utf8');
+			const dom = new JSDOM(fileContents);
 
+			const correctThese = dom.window.document.querySelectorAll('text[line-spacing]');
+			correctThese.forEach((correctThis) => {
+				const fontSize = correctThis.getAttribute('font-size');
+				const lineSpacing = correctThis.getAttribute('line-spacing');
+				const difference = fontSize - lineSpacing;
+				const distance = Math.ceil(Math.abs(difference));
+				const adjust = (distance > 4 && difference >= 0) ? distance : 4;
+				const finalAdjust = (difference >= 0) ? adjust : (distance <= fontSize) ? -1 : -adjust;
+				console.log(difference, finalAdjust);
+				correctThis.querySelectorAll('tspan').forEach((t) => {
+					t.setAttribute('y', t.getAttribute('y') - finalAdjust);
+				});
+			});
 
+			fs.writeFileSync(path.join(fileOrDirectoryPath, 'main-svgo-processed', subFileOrDirectory), dom.serialize(), 'utf8');
+		});
 
 		console.log(`mkdir -p ${fileOrDirectoryPath}/main && mv ${fileOrDirectoryPath}/main-svgo-processed/* ${fileOrDirectoryPath}/main/`);
 		execSync(`mkdir -p ${fileOrDirectoryPath}/main && mv ${fileOrDirectoryPath}/main-svgo-processed/* ${fileOrDirectoryPath}/main/`);
