b4c869012bcd3e89032209d9770a4c5613a81ebe
Christopher McCulloh <cmcculloh@salesforce.com>
2019-09-20 16:01:13 -0400

    updates config

--- config/config.sh
+++ config/config.sh
@@ -8,6 +8,7 @@ cp config/demos/config.$1.$2.$3.json config/config.json
 # npm run set-config audience-360 cnx19keynote fever
 # npm run set-config is cnx19keynote michael-page
 # npm run set-config audience-360 cnx19keynote pacers
+# npm run set-config is basecamp19 msm
 
 
 # eventually this should totally be a menu that let's you select this stuff. Maybe even on startup...
