5368f0683a094c28a30689b6f73e17fc30667771
Chris Corwin <ccorwin@salesforce.com>
2019-10-07 13:36:13 -0400

    Changes steps to short @AAFAST syntax

--- engine/javascripts/magick-flows.js
+++ engine/javascripts/magick-flows.js
@@ -245,7 +245,18 @@ function locationHashChanged(event) {
 
 	useStepTransition = currentStepMetaData.find(k => k=='use-step-transition') === 'use-step-transition';
 	doApplicationSwitchStepTransition = currentStepMetaData.find(k => k=='step-transition_app-switch') === 'step-transition_app-switch';
-	doAutoAdvanceStepTransition =  currentStepMetaData.find(k => k=='step-transition_auto-advance') === 'step-transition_auto-advance';
+	doAutoAdvanceStepTransition = currentStepMetaData.find(k => k=='step-transition_auto-advance') === 'step-transition_auto-advance';
+	doAutoAdvanceStepTransition_altSyntax1 = currentStepMetaData.find(k => k=='@AAXFAST') === '@AAXFAST';
+	doAutoAdvanceStepTransition_altSyntax2 = currentStepMetaData.find(k => k=='@AAFAST') === '@AAFAST';
+	doAutoAdvanceStepTransition_altSyntax3 = currentStepMetaData.find(k => k=='@AA') === '@AA';
+	doAutoAdvanceStepTransition_altSyntax4 = currentStepMetaData.find(k => k=='@AASLOW') === '@AASLOW';
+
+	console.log(`currentStepMetaData: `, currentStepMetaData);
+
+
+	if (doAutoAdvanceStepTransition_altSyntax1 === true || doAutoAdvanceStepTransition_altSyntax2 === true || doAutoAdvanceStepTransition_altSyntax3 === true || doAutoAdvanceStepTransition_altSyntax4 === true) {
+		doAutoAdvanceStepTransition = true;
+	}
 
 
 	// console.group(`[ ${magickFlowConfig.urlSlug} ☞ #${previousStepNumber} location hash changed to: #${currentStepNumber} ]`);
@@ -299,15 +310,26 @@ function locationHashChanged(event) {
 
 
 
-	if ( doAutoAdvanceStepTransition === true ) {
 		console.debug(`doAutoAdvanceStepTransition: `, doAutoAdvanceStepTransition);
+	if ( doAutoAdvanceStepTransition === true ) {
 
 		if ( currentStepMetaData.find(k => k=='step-transition-timing--slow') === 'step-transition-timing--slow') {
+			autoAdvanceTransitionTiming = 5000;
+		}
+		if ( currentStepMetaData.find(k => k=='@AASLOW') === '@AASLOW') {
 			autoAdvanceTransitionTiming = 5000;
 		}
 		if ( currentStepMetaData.find(k => k=='step-transition-timing--fast') === 'step-transition-timing--fast') {
 			autoAdvanceTransitionTiming = 100;
 		}
+		if ( currentStepMetaData.find(k => k=='@AAFAST') === '@AAFAST') {
+			autoAdvanceTransitionTiming = 100;
+		}
+		if ( currentStepMetaData.find(k => k=='@AAXFAST') === '@AAXFAST') {
+			autoAdvanceTransitionTiming = 5;
+		}
+
+
 
 		let autoAdvanceTransitionStepNumber = stepToEvaluateForAppTransition;
 		if ( directionOfNavigation === 'forward' ) {
